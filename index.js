1ef28193eb8f531cfde8a82a5ca476a9b58b5ec5
Your Name <robertcjohnson1984@gmail.com>
2021-08-09 21:33:28 -0600

    add employee function with questions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // runs the application
-const mysql = require("mysql");
 const inquirer = require("inquirer");
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+
 
 function manager() {
     //Manager info
@@ -47,3 +48,63 @@ function manager() {
 };
 
 manager();
+
+function employee() {
+    inquirer
+        .prompt([ //What kind of employee to enter
+            {
+                type: 'list',
+                name: 'employeeType',
+                message: 'What kind of employee are you entering?',
+                choices: ["Engineer", "Intern", "Finished"],
+            },
+        ]).then((response) => {
+
+            if (response.employeeType === "Finished") {
+                console.log("Thank you! Your information has been updated.");
+            }
+            //Engineer questions
+            else if (response.employeeType === "Engineer") {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'employeeName',
+                            message: 'What is the employee name?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'employeeId',
+                            message: 'Please assign a new employee number with the format e###',
+                        },
+                        {
+                            type: 'input',
+                            name: 'employeeEmail',
+                            message: 'What is the employee email address?',
+                            validate: function (input) {
+                                let test = validator.validate(input)
+                                if (test === false) {
+                                    console.log("Please enter a valid email.")
+                                }
+                                return test
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'employeeLink',
+                            message: 'What is the GitHub user name for this employee?',
+                        },
+                    ]).then((response) => {
+                        //run response through class Engineer
+                        let newEmployee = new Engineer(response.employeeName, response.employeeId, response.employeeEmail, response.employeeLink);
+
+                        //push new info to team file using the id as a unique variable name
+                        const generateCard = teamHTML(newEmployee);
+                        fs.appendFileSync("./src/team.js", `\n${generateCard}`);
+
+                        //restart question loop for next employee
+                        employee();
+                    });
+                };
+            });
+    };

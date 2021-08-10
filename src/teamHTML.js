4db9af68fb0601cc03131d7aecb33e51bcce6a01
Your Name <robertcjohnson1984@gmail.com>
2021-08-09 22:35:44 -0600

    create engineer card data

--- src/teamHTML.js
+++ src/teamHTML.js
@@ -22,5 +22,24 @@ function teamHTML(newEmployee) {
     $(".card-deck").append(${newEmployee.employeeId})`
         );
     }
+    else if (newEmployee.employeeRole === "Engineer") {
+
+        return (`
+        const ${newEmployee.employeeId} = \`
+        <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+        <h2>${newEmployee.employeeName}</h2>
+        <h3>${newEmployee.employeeRole}</h3>
+        </div>  
+        <div class='card-body'>    
+        <p class='card-text'>Employee ID: ${newEmployee.employeeId}</p>
+        <p class='card-text'>Email: <a href='mailto:${newEmployee.employeeEmail}'>${newEmployee.employeeEmail}</a></p>
+        <p class='card-text'>GitHub: <a href='${newEmployee.employeeLink}' target='_blank'>${newEmployee.employeeLink}</a></p>
+        </div>
+        </div>\`
+        $(".card-deck").append(${newEmployee.employeeId})`
+        );
+    }
 }
+
 module.exports = teamHTML;
